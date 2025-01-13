a75512a1dae1c640112539f2e89c8a3ea9cc9cdf
Akeka_ <vladvaledol@gmail.com>
2025-01-13 20:01:08 +0300

    sys

--- Ex2/src/main.py
+++ Ex2/src/main.py
@@ -1,5 +1,10 @@
+import sys
+from pathlib import Path
+
 import uvicorn
 from fastapi import FastAPI
+
+sys.path.append(str(Path(__file__).parent.parent))
 
 from src.api.Api_v1 import router as router_hotels
 
