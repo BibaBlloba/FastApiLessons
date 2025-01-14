2fd9cf0629d1d568cb6ed1b6147297a2ef828ffe
Akeka_ <vladvaledol@gmail.com>
2025-01-14 15:42:31 +0300

    Alembic add rooms

--- Ex2/src/migrations/env.py
+++ Ex2/src/migrations/env.py
@@ -6,6 +6,7 @@ from sqlalchemy import engine_from_config, pool
 from src.config import settings
 from src.database import Base
 from src.models.hotels import HotelsOrm
+from src.models.rooms import RoomsOrm
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
