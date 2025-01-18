ed7b71b1df78075cd2fc77fb7be422f83c840ed7
Akeka_ <vladvaledol@gmail.com>
2025-01-18 22:09:07 +0300

    pagination

--- Ex2/src/api/Api_v1.py
+++ Ex2/src/api/Api_v1.py
@@ -21,30 +21,14 @@ async def get_hotels(
     location: str | None = Query(default=None),
     title: str | None = Query(default=None),
 ):
-
+    per_page = pagination.per_page or 5
     async with async_session_maker() as session:
-        return await HotelsRepository(session).get_all()
-
-    # per_page = pagination.per_page or 5
-    #
-    # async with async_session_maker() as session:
-    #     query = select(HotelsOrm)
-    #     if location:
-    #         query = query.filter(
-    #             func.lower(HotelsOrm.location).contains(location.strip().lower())
-    #         )
-    #     if title:
-    #         query = query.filter(
-    #             func.lower(HotelsOrm.title).contains(title.strip().lower())
-    #         )
-    #     query = query.limit(per_page).offset(per_page * (pagination.page - 1))
-    #
-    #     print(query.compile(engine, compile_kwargs={"literal_binds": True}))
-    #
-    #     result = await session.execute(query)
-    #
-    #     all = result.scalars().all()
-    #     return all
+        return await HotelsRepository(session).get_all(
+            location=location,
+            title=title,
+            limit=per_page,
+            offset=per_page * (pagination.page - 1),
+        )
 
 
 # POST
