f1011bfa4bc44e4be06c9ab7b23a7f0301243c3a
Akeka_ <vladvaledol@gmail.com>
2025-01-31 22:43:18 +0300

    WIP

---
+++ AuthExample/frontend/src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import AuthContainer from "./AuthContainer"
+
+const Hero = () => {
+  return (
+    <div className="flex flex-row justify-center items-center min-h-screen min-w-screen bg-gray-200 dark:bg-gray-800">
+      <AuthContainer />
+    </div>
+  )
+}
+
+export default Hero
