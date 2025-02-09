5ea257661a4bd368f920dd8679fc13821566646b
Akeka_ <vladvaledol@gmail.com>
2025-02-09 18:13:07 +0300

    repo's utils file

--- Ex2/src/repos/rooms.py
+++ Ex2/src/repos/rooms.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel
 from sqlalchemy import delete, func, insert, select, update
 
 from database import engine
+from repos.utils import rooms_ids_for_booking
 from schemas.rooms import Room
 from src.models.bookings import BookingsOrm
 from src.models.rooms import RoomsOrm
@@ -43,37 +44,6 @@ class RoomsRepository(BaseRepository):
         date_from: date,
         date_to: date,
     ):
-        rooms_count = (
-            select(BookingsOrm.room_id, func.count("*").label("rooms_booked"))
-            .select_from(BookingsOrm)
-            .filter(
-                BookingsOrm.date_from <= date_to,
-                BookingsOrm.date_to >= date_from,
-            )
-            .group_by(BookingsOrm.room_id)
-            .cte(name="rooms_booked")
-        )
-        rooms_left_table = (
-            select(
-                RoomsOrm.id.label("room_id"),
-                (RoomsOrm.quanity - func.coalesce(rooms_count.c.rooms_booked, 0)).label(
-                    "rooms_left"
-                ),
-            )
-            .select_from(RoomsOrm)
-            .outerjoin(rooms_count, RoomsOrm.id == rooms_count.c.room_id)
-            .cte(name="rooms_left_table")
-        )
-        rooms_ids_for_hotel = (
-            select(RoomsOrm.id).filter_by(hotel_id=hotel_id).subquery()
-        )
-        rooms_ids_to_get = (
-            select(rooms_left_table.c.room_id)
-            .select_from(rooms_left_table)
-            .filter(
-                rooms_left_table.c.rooms_left > 0,
-                rooms_left_table.c.room_id.in_(select(rooms_ids_for_hotel)),
-            )
-        )
+        rooms_ids_to_get = rooms_ids_for_booking(hotel_id, date_from, date_to)
 
         return await self.get_filtered(RoomsOrm.id.in_(rooms_ids_to_get))
