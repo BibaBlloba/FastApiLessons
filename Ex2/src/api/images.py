8e5cc36a7cb9e0f96d90f1e55097651656f3633b
Akeka_ <vladvaledol@gmail.com>
2025-03-06 17:02:11 +0300

    celery images scaling

---
+++ Ex2/src/api/images.py
@@ -0,0 +1,21 @@
+import shutil
+
+from fastapi import APIRouter, UploadFile
+
+from src.tasks.tasks import resize_and_save_image
+
+router = APIRouter(prefix="/images", tags=["Изображения"])
+
+
+@router.get("")
+async def get_images():
+    return {"status": "ok"}
+
+
+@router.post("")
+async def upload_image(file: UploadFile):
+    image_path = f"src/static/images/{file.filename}"
+    with open(image_path, "wb+") as new_file:
+        shutil.copyfileobj(fsrc=file.file, fdst=new_file)
+
+        resize_and_save_image.delay(image_path, file.filename)
--- Ex2/src/tasks/tasks.py
+++ Ex2/src/tasks/tasks.py
@@ -1,4 +1,7 @@
+import os
 from time import sleep
+
+from PIL import Image
 
 from src.tasks.celery_app import celery_instance
 
@@ -7,3 +10,31 @@ from src.tasks.celery_app import celery_instance
 def test_task():
     sleep(5)
     print("Найс")
+
+
+@celery_instance.task
+def resize_and_save_image(image_path, filename):
+    # Открытие исходного изображения
+    with Image.open(image_path) as img:
+        # Список целевых ширин
+        output_dir = "src/static/images"
+        target_widths = [1000, 500, 300]
+
+        # Получаем исходную высоту и ширину
+        original_width, original_height = img.size
+
+        # Для каждой целевой ширины
+        for width in target_widths:
+            # Расчёт нового соотношения сторон
+            aspect_ratio = original_height / original_width
+            new_height = int(width * aspect_ratio)
+
+            # Изменяем размер изображения
+            resized_img = img.resize((width, new_height))
+
+            # Формируем путь для сохранения
+            output_path = os.path.join(output_dir, f"{width}_{filename}")
+
+            # Сохраняем изображение
+            resized_img.save(output_path)
+            print(f"Изображение сохранено: {output_path}")
